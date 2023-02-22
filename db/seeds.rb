a3f9a85db794e45bacc6f3e90e351ae9cea9ad7d
Rachel Jones <rachelscjones@gmail.com>
2023-02-22 16:10:53 +0000

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'creating some seed flats'
+Flat.destroy_all
+puts 'destroyed any previously created flats'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Luxury Romantic Cottage - Idyllic Rural Bliss',
+  address: 'Northamptonshire, England, United Kingdom',
+  description: 'Beautiful Romantic Country Cottage with private deck offering amazing rolling countryside views set in 14 acre estate.',
+  price_per_night: 250,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Period Pimlico hideaway (self contained annexe)',
+  address: 'Pimlico, London',
+  description: 'Classic, comfortable, private self contained  ground floor annexe with own entrance in delightful period building.',
+  price_per_night: 99,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Loft NYC style inside a classy Victorian House',
+  address: 'Notting Hill, London',
+  description: 'AYour own private mini loft in one of the most exclusive streets of Notting Hill. Newly renovated apartment & building.',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+
+puts 'seed flats created!'
